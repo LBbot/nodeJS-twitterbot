c26faf68c3f3a7fca9a10c09086292d6cbf41ebe
LB <LBbot@users.noreply.github.com>
2017-08-17 15:58:55 +0100

    Tidying

--- lbbotbot.js
+++ lbbotbot.js
@@ -10,14 +10,14 @@ var config = require("./keys.js");
 var Twitter = new twit(config);
 
 function tweet(actualMessage) {
-	console.log("Attempting to tweet:")
-	console.log (actualMessage)
-	Twitter.post("statuses/update", {status: actualMessage}, function(error, tweet, response){
-		if(error){
-		console.log(error);
-		}
-	});
-	console.log("Tweet successfully Tweeted!");
+    console.log("Attempting to tweet:")
+    console.log (actualMessage)
+    Twitter.post("statuses/update", {status: actualMessage}, function(error, tweet, response){
+        if(error){
+        console.log(error);
+        }
+    });
+    console.log("Tweet successfully Tweeted!");
 }
 
 
@@ -26,43 +26,44 @@ var earth_status = function() {
     var today = new Date();
     tweet("It's " + today.getHours() + ":" + today.getMinutes() + " and the Earth is still here!");
 }
+
 earth_status();
 setInterval(earth_status, 60000);
 
 
 // Finds a random tweet with #Earth, Likes it and Retweets:
 var favoriteTweet = function(){  
-  var params = {
-      q: "#earth, #Earth",
-      result_type: "recent",
-      lang: "en"
-  }
-  // find the tweet
-  Twitter.get("search/tweets", params, function(err,data){
+    var params = {
+        q: "#earth, #Earth",
+        result_type: "recent",
+        lang: "en"
+    }
+    // find the tweet
+    Twitter.get("search/tweets", params, function(err,data){
     var tweet = data.statuses;
     var randomTweet = ranDom(tweet);   // pick a random tweet
     // if random tweet exists
     if(typeof randomTweet != "undefined"){
-      // Tell Twitter to Like
-      Twitter.post("favorites/create", {id: randomTweet.id_str}, function(err, response){
+        // Tell Twitter to Like
+        Twitter.post("favorites/create", {id: randomTweet.id_str}, function(err, response){
         // if there was an error while Liking
-        if(err){
-          console.log("CANNOT BE LIKED... Error");
-        }
-        else{
-          console.log("Random message LIKED!");
-        }
-      });
+            if(err){
+                console.log("CANNOT BE LIKED... Error");
+            }
+            else{
+                console.log("Random message LIKED!");
+            }
+        });
       
-      // TODO: This Retweets the random message, can probably be merged into the above function somehow...
-      Twitter.post("statuses/retweet/:id", {id: randomTweet.id_str}, function(err, response){
-        if(err){
-          console.log("CANNOT BE RETWEETED... Error");
-        }
-        else{
-          console.log("Random message RETWEETED!!!");
-        }
-      });
+        // This Retweets the random message, consider merging into the function above.
+        Twitter.post("statuses/retweet/:id", {id: randomTweet.id_str}, function(err, response){
+            if(err){
+                console.log("CANNOT BE RETWEETED... Error");
+            }
+            else{
+                console.log("Random message RETWEETED!!!");
+            }
+        });
     }
   });
 }
@@ -71,10 +72,11 @@ var favoriteTweet = function(){
 favoriteTweet();  
 setInterval(favoriteTweet, 60000);
 
+
 // function to generate a random tweet
 function ranDom (arr) {  
-  var index = Math.floor(Math.random()*arr.length);
-  return arr[index];
+    var index = Math.floor(Math.random()*arr.length);
+    return arr[index];
 };
 
 
@@ -98,15 +100,14 @@ function tweetNow(tweetTxt) {
         status: tweetTxt
     }
     Twitter.post("statuses/update", tweet, function(err, data, response) {
-      if(err){
-        console.log("Error in Replying");
-      }
-      else{
-        console.log("Follower acknowledged!");
-      }
+        if(err){
+            console.log("Error in Replying");
+        }
+        else{
+            console.log("Follower acknowledged!");
+        }
     });
 }
 
-// TODO: Tidy this up.
 
 // TODO: Implement Markov chain tweets eventually!
